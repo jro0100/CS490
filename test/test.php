d9d448a69bedb47e4cf92308285bc97653da47de
Nick <nhoffmann117@gmail.com>
2021-10-11 21:37:14 -0400

    Testing php exec() in heroku

--- test/test.php
+++ test/test.php
@@ -5,12 +5,12 @@ $questionID = 5;
 $_POST["functionToCall"] = "addtwo";
 
 $out = array();
-exec("mkdir testdir");
+exec("echo test", $out);
 //exec("./maketestcasefile.sh 15 addfour");
 //exec("./populateunittests.sh 1 5 4 3 addfour 13 1 15", $out);
 //exec("./populateunittests.sh 72 3 65 19 addfour 159 2 15", $out);
 //exec("./addclosingtag.sh 15");
-//var_export($out);
+var_export($out);
 //echo "it work?";
 ?>
 
