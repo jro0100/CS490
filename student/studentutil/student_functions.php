aa75ecb3dd291bab4b946612e85787aab3669918
Nick Hoffmann <nhoffmann117@gmail.com>
2021-10-12 19:21:29 -0400

    Set invalid login redirects to go to logout.php

--- student/studentutil/student_functions.php
+++ student/studentutil/student_functions.php
@@ -1,7 +1,7 @@
 <?php
 function redirect_to_login_if_not_valid_student() {
     if (!isset($_SESSION["logged_student"]) || $_SESSION["logged_student"] != true) {
-        header("Location: ../");
+        header("Location: ../logout.php");
         exit();
     }
 }
