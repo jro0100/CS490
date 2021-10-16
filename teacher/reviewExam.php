aa6e29e7cc9a22de5c5b536ab1b024faa25f2e97
Justin <jro0100@gmail.com>
2021-10-16 09:31:42 -0400

    Updated menu bar on reviewExams page

--- teacher/reviewExam.php
+++ teacher/reviewExam.php
@@ -8,8 +8,8 @@
     <body>
         <nav class="navbar">
             <ul class="nav-links">
-                <li class="nav-item"><a href="index.php">Oustanding Exams</a></li>
-                <li class="nav-item"><a href="grades.php">Grades</a></li>
+                <li class="nav-item"><a href="index.php">Question Bank</a></li>
+                <li class="nav-item"><a href="exams.php">Exams</a></li>
                 <li class="nav-item"><a href="../logout.php">Logout</a></li>
             </ul>
         </nav>
