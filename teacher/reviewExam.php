4e9c82cce03e17a4bd82e47abbfd7578a85968be
Justin <jro0100@gmail.com>
2021-10-15 23:34:04 -0400

    Added dropdown students

---
+++ teacher/reviewExam.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet"  href="../css/menu.css">
+        <link rel="stylesheet"  href="../css/main.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li class="nav-item"><a href="index.php">Oustanding Exams</a></li>
+                <li class="nav-item"><a href="grades.php">Grades</a></li>
+                <li class="nav-item"><a href="../logout.php">Logout</a></li>
+            </ul>
+        </nav>
+
+        <label for="student" style="margin-top:30px">Students</label>
+            <select name="student" id="student" style="margin-top:30px">
+                <!-- <option> Enter code here </option> -->
+            </select>
+    </body>
+</html>
