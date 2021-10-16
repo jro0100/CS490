68a1e34b7e27f836c3235fcc9bae19562c2daf7c
Nick <nhoffmann117@gmail.com>
2021-10-16 14:17:30 -0400

    Changed from GET to POST

--- teacher/saveChanges.php
+++ teacher/saveChanges.php
@@ -4,22 +4,22 @@ require("./teacherutil/teacher_functions.php");
 require("../util/functions.php");
 redirect_to_login_if_not_valid_teacher();
 
-if (!isset($_GET["saveChanges"]) || !isset($_GET["examID"]) || !isset($_GET["studentID"])) {
+if (!isset($_POST["saveChanges"]) || !isset($_POST["examID"]) || !isset($_POST["studentID"])) {
     header("Location: exams.php");
     exit();
 }
 
-$studentID = $_GET["studentID"];
-$examID = $_GET["examID"];
+$studentID = $_POST["studentID"];
+$examID = $_POST["examID"];
 
-unset($_GET["saveChanges"]);
-unset($_GET["studentID"]);
-unset($_GET["examID"]);
+unset($_POST["saveChanges"]);
+unset($_POST["studentID"]);
+unset($_POST["examID"]);
 
 $sqlstmt = "UPDATE questiongrade SET achievedPoints = :achievedPoints, teacherComment = :teacherComment WHERE studentID = :studentID AND examID = :examID AND questionID = :questionID";
 $params = array();
 $tempArray = array();
-foreach ($_GET as $key => $value) {
+foreach ($_POST as $key => $value) {
     $splitKey = explode("-", $key);
     $field = ":" . $splitKey[0];
     $qID = $splitKey[1];
