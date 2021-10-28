cdf7a6eafc3efb9667b049fd31656c8512835301
Justin <jro0100@gmail.com>
2021-10-27 21:57:00 -0400

    Made UI for adding questions to exam

--- teacher/newCreateExam.php
+++ teacher/newCreateExam.php
@@ -35,28 +35,30 @@ if ($result) {
         </ul>
     </nav>
 
-    <div class="row">
-        <div class="center-column-text">
-            <div class="column-minus-color">
+    <div class="float-container">
+        <div class="float-child" id="leftCol">
+            <div class="center-column-text-header">
                 Question Bank
             </div>
         </div>
-        <div class="center-column-text">
-            <div class="column-minus-color" class="center-column-text">
-                Selected Questions
+            <div class="float-child" id="rightCol">
+                    <div class="center-column-text-header">
+                        Selected Questions
+                    </div>
             </div>
-        </div>
     </div>
 
 
     <script>
         var text = <?php echo $json ?>;
-        form = document.createElement("form");
-        form.setAttribute("method", "post");
-        form.setAttribute("action", "createExam.php");
+
+        rightForm = document.createElement("form");
+        rightForm.setAttribute("id", "rightForm");
+        rightColumn = document.getElementById("rightCol");
+        rightColumn.appendChild(rightForm);
 
         for (i = 0; i < text.length; i++) {
-
+            
             const obj = JSON.parse(JSON.stringify(text[i]));
 
             difficulty = "";
@@ -68,55 +70,65 @@ if ($result) {
                 difficulty = "Hard";
             }
 
-            //Create row
-            row = document.createElement("div");
-            row.classList.add("row");
+            leftQuestion = document.createElement("div");
+            leftQuestion.setAttribute("id", "l" + i);
 
-            //Create column
-            column = document.createElement("div");
-            column.classList.add("column");
-
-            aTag = document.createElement("a");
-            aTag.setAttribute("href", "question.php?questionID=" + obj.questionID);
+            centerDiv = document.createElement("div");
+            centerDiv.classList.add("center-column-text");
 
             question = document.createElement("p");
-            question.classList.add("center-column-text");
+            question.classList.add("center-text");
             question.innerHTML = obj.question;
 
             typeAndDif = document.createElement("p");
-            typeAndDif.classList.add("center-column-text");
+            typeAndDif.classList.add("center-text");
             typeAndDif.innerHTML = "Type: " + obj.questionType + "&emsp;" + "Difficulty: " + difficulty;
 
-            input = document.createElement("input");
-            input.setAttribute("type", "checkbox");
-            input.setAttribute("class", "check");
-            input.setAttribute("name", i);
-            input.setAttribute("value", obj.questionID);
+            checkBox = document.createElement("input");
+            checkBox.setAttribute("type", "checkbox");
+            checkBox.setAttribute("class", "check");
+            checkBox.setAttribute("id", "checkBox");
+            checkBox.setAttribute("onclick", "checkBoxClicked();")
+            checkBox.setAttribute("value", obj.questionID);
+            checkBox.setAttribute("name", obj.questionID);
 
-            aTag.appendChild(question);
-            aTag.appendChild(typeAndDif);
-            aTag.appendChild(input);
-            column.appendChild(aTag);
-            row.appendChild(column);
-            form.appendChild(row);
+            leftCol = document.getElementById("leftCol");
+            
+            centerDiv.appendChild(question);
+            centerDiv.appendChild(typeAndDif);
+            centerDiv.appendChild(checkBox);
+            leftQuestion.appendChild(centerDiv);
+            leftCol.appendChild(leftQuestion);
         }
-        buttonDiv = document.createElement("div");
-        buttonDiv.classList.add("center");
-        createExamButton = document.createElement("input");
-        createExamButton.setAttribute("type", "submit");
-        createExamButton.setAttribute("class", "submitButton");
-        createExamButton.setAttribute("name", "create");
-        createExamButton.value = "Submit";
-        buttonDiv.appendChild(createExamButton);
-        form.appendChild(buttonDiv);
 
-        if (text.length == 0) {
-            emptiness = document.createElement("div");
-            emptiness.classList.add("center-column-text");
-            emptiness.innerHTML = "NO QUESTIONS EXIST YET!";
-            document.body.appendChild(emptiness);
-        } else {
-            document.body.appendChild(form); //Appends the div to the body of the HTML page
+        function checkBoxClicked() {
+            leftChildren = document.getElementById("leftCol").children;
+            for(i = 1; i < leftChildren.length; i++) {
+                input = leftChildren[i].getElementsByTagName("input");
+                for(y = 0; y < input.length; y++) {
+                    if(input[y].checked) {
+                        pointVal = document.createElement("input");
+                        pointVal.setAttribute("type", "text");
+                        pointVal.setAttribute("name", input[y].name);
+                        pointVal.setAttribute("placeholder", "Point Value");
+                        pointVal.setAttribute("pattern", "^[1-9][0-9]*$");
+                        pointVal.classList.add("point-val-field");
+                        rightCol = document.getElementById("rightCol");
+                        leftChildren[i].firstChild.appendChild(pointVal);
+                        rightForm.appendChild(leftChildren[i]);
+                    }
+                }
+            }
+
+            rightChildren = document.getElementById("rightForm").children;
+            for(i = 0; i < rightChildren.length; i++) {
+                input = rightChildren[i].getElementsByTagName("input");
+                if(input[0].checked == false && input[0].id == "checkBox") {
+                    input[1].parentNode.removeChild(input[1]);
+                    leftCol = document.getElementById("leftCol");
+                    leftCol.appendChild(rightChildren[i]);
+                }
+            }
         }
     </script>
 </body>
